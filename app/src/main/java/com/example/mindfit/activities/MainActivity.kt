6508fb5bde8879ff9eaf281e886e2969efd5a760
Thiagom97092 <arleymaya25@gmail.com>
2025-05-21 16:55:14 -0500

    Navegando entre pantallas Welcome y Login

--- app/src/main/java/com/example/mindfit/activities/MainActivity.kt
+++ app/src/main/java/com/example/mindfit/activities/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.mindfit.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mindfit.R
+import com.example.mindfit.fragments.WelcomeFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // asegúrate que este layout existe
+        setContentView(R.layout.activity_main)
+
+        // Mostrar el fragmento de bienvenida al iniciar
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, WelcomeFragment())
+            .commit()
     }
 }
--- app/src/main/java/com/example/mindfit/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/example/mindfit/fragments/WelcomeFragment.kt
@@ -1,17 +1,35 @@
 package com.example.mindfit.fragments
 
-import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.example.mindfit.R
+import com.example.mindfit.fragments.LoginFragment
+
 
 class WelcomeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val botonEmpezar = view.findViewById<Button>(R.id.btn_empezar)
+
+        botonEmpezar.setOnClickListener {
+            // Aquí haces el cambio de fragmento
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, LoginFragment()) // El ID del contenedor debe estar en tu activity_main.xml
+                .addToBackStack(null) // Permite volver atrás con el botón
+                .commit()
+        }
     }
 }
